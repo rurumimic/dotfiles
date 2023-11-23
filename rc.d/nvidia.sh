eb30a2caccf0211e8df00fa4b50146bfbfd03047
rurumimic <10915235+rurumimic@users.noreply.github.com>
2023-11-23 20:02:58 +0900

    cuda toolkit 12.3

---
+++ rc.d/nvidia.sh
@@ -0,0 +1,10 @@
+### Nvidia ###
+
+# CUDA Tookit 12.3
+
+if [ -d "/usr/local/cuda-12.3/bin" ]; then
+    export PATH="$PATH:/usr/local/cuda-12.3/bin"
+    export LD_LIBRARY_PATH=/usr/local/cuda-12.3/lib64\
+                         ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+fi
+

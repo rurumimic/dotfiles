138ffa7852b85a58a686b533cbb3e024329a9ab8
rurumimic <10915235+rurumimic@users.noreply.github.com>
2024-04-13 13:24:04 +0900

    vagrant default provider: virtualbox

--- rc.d/vagrant.sh
+++ rc.d/vagrant.sh
@@ -1,31 +1,34 @@
 ### Vagrant ###
 
+if command -v virtualbox >/dev/null; then
+	export VAGRANT_DEFAULT_PROVIDER=virtualbox
+fi
+
 vsh() {
 
-    if [ ! -f Vagrantfile ]; then
-        echo -e "\e[31mThere is no 'Vagrantfile' here.\e[0m" >&2
-        return 1
-    fi
+	if [ ! -f Vagrantfile ]; then
+		echo -e "\e[31mThere is no 'Vagrantfile' here.\e[0m" >&2
+		return 1
+	fi
 
-    if [ ! -d .vagrant ]; then
-        echo -e "\e[31mThere is no '.vagrant/' here.\e[0m" >&2
-        return 1
-    fi
+	if [ ! -d .vagrant ]; then
+		echo -e "\e[31mThere is no '.vagrant/' here.\e[0m" >&2
+		return 1
+	fi
 
-    if [ ! -f .vagrant/ssh-config ]; then
-        vagrant ssh-config > .vagrant/ssh-config
-    fi
+	if [ ! -f .vagrant/ssh-config ]; then
+		vagrant ssh-config >.vagrant/ssh-config
+	fi
 
-    if [ ! -f .vagrant/ssh-config ] || [ ! -s .vagrant/ssh-config ]; then
-        vagrant ssh-config > .vagrant/ssh-config
-    fi
+	if [ ! -f .vagrant/ssh-config ] || [ ! -s .vagrant/ssh-config ]; then
+		vagrant ssh-config >.vagrant/ssh-config
+	fi
 
-    if [ ! -s .vagrant/ssh-config ]; then
-        echo -e "\e[31mCould not generate '.vagrant/ssh-config'.\e[0m" >&2
-        return 1
-    fi
+	if [ ! -s .vagrant/ssh-config ]; then
+		echo -e "\e[31mCould not generate '.vagrant/ssh-config'.\e[0m" >&2
+		return 1
+	fi
 
-    kitty +kitten ssh -F .vagrant/ssh-config "${1:-default}"
+	kitty +kitten ssh -F .vagrant/ssh-config "${1:-default}"
 
 }
-

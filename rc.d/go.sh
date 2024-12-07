691fb7a11cd5ba6f1e24804a7da3efc1587ccea8
rurumimic <10915235+rurumimic@users.noreply.github.com>
2024-12-07 19:52:19 +0900

    add go user path

--- rc.d/go.sh
+++ rc.d/go.sh
@@ -1,7 +1,8 @@
 ### Go ###
 
 if [ -d "/usr/local/go/bin" ]; then
-    export PATH="$PATH:/usr/local/go/bin"
     export GOPATH="$HOME/.go"
+    export PATH="$PATH:/usr/local/go/bin"
+    export PATH="$PATH:$(go env GOPATH)/bin"
 fi
 

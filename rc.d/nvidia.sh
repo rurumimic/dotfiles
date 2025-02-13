911f95bcda02512ceee7837a3a6a5fea57d8d067
rurumimic <10915235+rurumimic@users.noreply.github.com>
2025-02-14 00:19:30 +0900

    nvidia cuda 12.x

--- rc.d/nvidia.sh
+++ rc.d/nvidia.sh
@@ -2,8 +2,8 @@
 
 # CUDA Tookit 12.3
 
-if [ -d "/usr/local/cuda-12.3/bin" ]; then
-    export PATH="$PATH:/usr/local/cuda-12.3/bin"
-    export LD_LIBRARY_PATH=/usr/local/cuda-12.3/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+if [ -d "/usr/local/cuda-12/bin" ]; then
+    export PATH="$PATH:/usr/local/cuda-12/bin"
+    export LD_LIBRARY_PATH=/usr/local/cuda-12/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 fi
 

de52cc66ecb08e78d52f79c1507ee1af88dd70b4
rurumimic <10915235+rurumimic@users.noreply.github.com>
2026-02-24 09:34:46 +0900

    deprecated pyenv, autoenv

--- rc.d/python.sh
+++ rc.d/python.sh
@@ -1,18 +1,18 @@
 ### Python ###
 
 # pyenv
-if [ -d "$HOME/.pyenv" ]; then
-    export PYENV_ROOT="$HOME/.pyenv"
-    command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
-fi
+# if [ -d "$HOME/.pyenv" ]; then
+#     export PYENV_ROOT="$HOME/.pyenv"
+#     command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+#     eval "$(pyenv init -)"
+#     eval "$(pyenv virtualenv-init -)"
+# fi
 
 # autoenv
-if [ -d "$HOME/.autoenv" ]; then
-    export AUTOENV_ENV_FILENAME='.autoenv'
-    export AUTOENV_ENV_LEAVE_FILENAME='.autoenv.leave'
-    export AUTOENV_ENABLE_LEAVE='enabled'
-    source "$HOME/.autoenv/activate.sh"
-fi
+# if [ -d "$HOME/.autoenv" ]; then
+#     export AUTOENV_ENV_FILENAME='.autoenv'
+#     export AUTOENV_ENV_LEAVE_FILENAME='.autoenv.leave'
+#     export AUTOENV_ENABLE_LEAVE='enabled'
+#     source "$HOME/.autoenv/activate.sh"
+# fi
 
